bcebe4081337a3f71b2ee4d6ca83654853f72712
Gilson Fernandes Batista de Souza <gilsonfernandesbatista@gmail.com>
2016-08-01 14:47:53 -0300

    Cross Origin Token

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -20,7 +20,7 @@ class CorsMiddleware
 
         return $next($request)->header('Access-Control-Allow-Origin', $allow)
             ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Origin');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Origin, X-CSRF-Token');
     }
 
     /**
