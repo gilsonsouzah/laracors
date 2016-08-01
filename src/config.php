e4f8c131bdb9d017c02090f8b433ac15bb598382
Gilson Fernandes Batista de Souza <gilsonfernandesbatista@gmail.com>
2016-08-01 09:39:27 -0300

    Initial Commit

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Configure the allowed domains to access cors service. Set the domains on
+ * permissions, in array format, or use * for allow all domains.
+ *
+ * @return Array[String] Allowed domains
+ */
+return [
+    'permissions' => '*',
+];
